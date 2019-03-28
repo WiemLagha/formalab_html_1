d2b37469427112b1bcac18409a10f634abab8d6d
WiemLagha <laghawiem98@gmail.com>
2019-03-28 10:36:33 +0100

    fonction carre

--- Js/test5.js
+++ Js/test5.js
@@ -1,8 +1,10 @@
 function message(){
     alert('Bonjour à tous!!!');
 }
-
+function carre(tNb){
+    return tNb.value*tNb.value;
+}
 function message2(t){
-    alert('Bonjour'+t.value);
+    alert('Bonjour '+t.value);
 
 }
